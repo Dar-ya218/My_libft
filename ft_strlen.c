5b2c4eb13aae22caa4ad13099e180e565fc07af1
dabochko <dabochko@student.42barcelona.com>
2024-01-17 13:48:03 +0100

    strlen with mein

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,20 +6,28 @@
 /*   By: dabochko <dabochko@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 15:22:17 by dabochko          #+#    #+#             */
-/*   Updated: 2024/01/17 11:01:39 by dabochko         ###   ########.fr       */
+/*   Updated: 2024/01/17 13:47:20 by dabochko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 size_t	ft_strlen(const char *s)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	if (!s)
-		return (0);
-	while (s[i]!= '\0')
+	while (s[i] != '\0')
 		i++;
-		return (i);
+	return (i);
 }
+/*
+int main(void)
+{
+	const char	*test_strlen;
+
+	test_strlen = "Hello world!";
+	printf("%zu\n", ft_strlen(test_strlen));
+	return (0);
+}*/
