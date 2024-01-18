2eb033ce1113f8e4806fdb8c669ed16f5f96cfc2
dabochko <dabochko@student.42barcelona.com>
2024-01-18 17:45:41 +0100

    memset done

--- ft_memset.c
+++ ft_memset.c
@@ -6,8 +6,36 @@
 /*   By: dabochko <dabochko@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 15:23:05 by dabochko          #+#    #+#             */
-/*   Updated: 2024/01/09 15:23:10 by dabochko         ###   ########.fr       */
+/*   Updated: 2024/01/18 12:45:49 by dabochko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+//# include <stdio.h>
 
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char *memory_ptr;
+	unsigned char value;
+
+	memory_ptr = b;
+	value = (unsigned char)c;
+	while (len > 0)
+	{
+		*memory_ptr = value;
+		memory_ptr++;
+		len--;	   
+	}
+	return (b);
+}
+/*
+int main(void)
+{
+	char b[10] = "Mundo";
+	char *res;
+
+	printf("string: %s\n", b);
+	res = ft_memset(b, 42, 3);
+	printf("mio_memset: %s\n", res);
+	return (0);
+}*/
---
+++ libft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabochko <dabochko@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/18 11:12:21 by dabochko          #+#    #+#             */
+/*   Updated: 2024/01/18 11:19:42 by dabochko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+
+# define LIBFT_H
+# include <stddef.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void *ft_memset(void *b, int c, size_t len);
+void *ft_bzero(void *s, size_t n);
+
+#endif
