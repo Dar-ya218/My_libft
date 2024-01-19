a7bf8db5a9abca87a6bac61b06ae49ee859030ed
dabochko <dabochko@student.42barcelona.com>
2024-01-19 14:01:58 +0100

    memset refactor

--- ft_memset.c
+++ ft_memset.c
@@ -6,36 +6,37 @@
 /*   By: dabochko <dabochko@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 15:23:05 by dabochko          #+#    #+#             */
-/*   Updated: 2024/01/18 12:45:49 by dabochko         ###   ########.fr       */
+/*   Updated: 2024/01/19 13:58:27 by dabochko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-//# include <stdio.h>
+#include <stdio.h>
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char *memory_ptr;
-	unsigned char value;
+	unsigned char	*memory_ptr;
+	unsigned char	value;
+	size_t			i;
 
-	memory_ptr = b;
+	i = 0;
+	memory_ptr = (unsigned char *);
 	value = (unsigned char)c;
-	while (len > 0)
+	while (i < len)
 	{
-		*memory_ptr = value;
-		memory_ptr++;
-		len--;	   
+		memory_ptr[i] = value;
+		i++;
 	}
 	return (b);
 }
-/*
+
 int main(void)
 {
-	char b[10] = "Mundo";
+	char b[] = "Mundo";
 	char *res;
 
 	printf("string: %s\n", b);
 	res = ft_memset(b, 42, 3);
 	printf("mio_memset: %s\n", res);
 	return (0);
-}*/
+}
