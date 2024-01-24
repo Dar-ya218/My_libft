fa7f336388fbfdf0f171bd5d6da339b99f94bfb6
Dar-ya218 <dasharoura13465@gmail.com>
2024-01-25 00:43:49 +0100

    strlcat done

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -9,3 +9,43 @@
 /*   Updated: 2024/01/23 13:31:54 by dabochko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+//#include <stdio.h>
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+	size_t	dst_l;
+	size_t	src_l;
+
+	dst_l = ft_strlen(dst);
+	src_l = ft_strlen(src);
+	i = dst_1;
+	j = 0;
+	if (dstsize <= dst_l)
+		return (src_l + dstsize);
+	while (src[j] != '\0' && i < dstsize - 1)
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+     if (i < dstsize)
+	    dst[i] = '\0';
+	return (dst_l + src_l);
+}
+/*int main() {
+    char src[50] = " World!";
+    char dest[50] = "Hello,";
+
+    size_t len = ft_strlcat(dest, src, 50);
+
+    printf("Length: %zu\n", len);
+    printf("Source: %s\n", src);
+    printf("Destination: %s\n", dest);
+
+    return 0;
+}
+*/
