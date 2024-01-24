04d4c4e4203c3b3354d42cd2752085c3f968339f
Dar-ya218 <dasharoura13465@gmail.com>
2024-01-25 00:33:56 +0100

    strlcpy done

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -9,3 +9,37 @@
 /*   Updated: 2024/01/23 13:33:08 by dabochko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+//#include <stdio.h>
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	len = ft_strlen(src);
+	if (dstsize == 0)
+		return (len);
+	while (src[i] != '\0' && i < dstsize - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (len);
+}
+/*
+int main() {
+    char src[50] = "Hello, World!";
+    char dest[50];
+
+    size_t len = ft_strlcpy(dest, src, 50);
+
+    printf("Length: %zu\n", len);
+    printf("Source: %s\n", src);
+    printf("Destination: %s\n", dest);
+
+    return 0;
+}*/
