c45af579dca78400c70b98dc65d9511240dbbe6f
Dar-ya218 <dasharoura13465@gmail.com>
2024-01-28 23:23:33 +0100

    memchr done

--- ft_memchr.c
+++ ft_memchr.c
@@ -10,4 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	void			*result;
+	size_t			bytes;
+	unsigned char	*uc_s;
+	short			found;
+	unsigned char	searched;
+
+	result = NULL;
+	if (n)
+	{
+		uc_s = (unsigned char *)s;
+		searched = (unsigned char)c;
+		bytes = 0;
+		found = 0;
+		while (bytes < n && !found)
+		{
+			if (uc_s[bytes] == searched)
+				found = 1;
+			else
+				bytes++;
+		}
+		if (found)
+			result = &uc_s[bytes];
+	}
+	return (result);
+}
