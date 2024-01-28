1df0bfe869f600d0b63568013a3b3adef82fd912
Dar-ya218 <dasharoura13465@gmail.com>
2024-01-28 23:53:20 +0100

    memcmp done

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -11,3 +11,33 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+//#include <stdio.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*uc_s1;
+	unsigned char	*uc_s2;
+	size_t			idx;
+
+	uc_s1 = (unsigned char *)s1;
+	uc_s2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n && uc_s1[i] == uc_s2[i])
+		i++;
+	if (i == n)
+		return (0);
+	else
+		return (uc_s1[i] - uc_s2[i]);
+}
+
+/*int main() {
+    const char *s1 = "Hello, World!";
+    const char *s2 = "Hello, world!";
+    size_t n = 7;
+
+    int result = ft_memcmp(s1, s2, n);
+
+    printf("Comparison result: %d\n", result);
+
+    return 0;
+}*/
