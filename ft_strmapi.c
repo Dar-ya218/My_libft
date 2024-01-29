2af05db79065982ac95d7fe9063a5257803587e4
Dar-ya218 <dasharoura13465@gmail.com>
2024-01-30 00:38:48 +0100

    add strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -10,4 +10,49 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+//#include <stdio.h>
 
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	s_len;
+	char	*t;
+	size_t	i;
+
+	if (!s || !f)
+		return (NULL);
+	s_len = ft_strlen(s);
+	t = (char *)malloc(s_len + 1);
+	if (t)
+	{
+		i = 0;
+		while (i < s_len)
+		{
+			t[i] = f(i, s[i]);
+			i++;
+		}
+		t[i] = '\0';
+	}
+	return (t);
+}
+
+/*
+char my_func(unsigned int i, char c) {
+    return c + i;
+}
+
+int main() {
+    const char *s = "Hello, World!";
+
+    char *result = ft_strmapi(s, my_func);
+
+    if (result) {
+        printf("Original string: \"%s\"\n", s);
+        printf("Mapped string: \"%s\"\n", result);
+        free(result);  // Don't forget to free the memory!
+    } else {
+        printf("Failed to allocate memory.\n");
+    }
+
+    return 0;
+}*/
