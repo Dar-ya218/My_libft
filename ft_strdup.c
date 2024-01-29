b4f6ce7862c07a18a1d103ee5d439ebb646a0fa7
Dar-ya218 <dasharoura13465@gmail.com>
2024-01-30 00:07:51 +0100

    add strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -11,3 +11,31 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+//#include <stdio.h>
+
+char	*ft_strdup(const char *s)
+{
+	size_t	s_size;
+	char	*dup;
+
+	s_size = ft_strlen(s);
+	dup = (char *)malloc(s_size + 1);
+	if (dup == NULL)
+	{
+		errno = ENOMEM;
+		return (NULL);
+	}
+	else
+		return ((char *)ft_memcpy(dup, s, s_size +1));
+}
+
+/*int main() {
+	const char *s = "Hello, World!";
+
+	char *result = ft_strdup(s);
+
+	printf("Original string: \"%s\"\n", s);
+	printf("Duplicated string: \"%s\"\n", result);
+
+	return 0;
+}*/
