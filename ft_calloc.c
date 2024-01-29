b5b9f6a13486d71c9f6c14de303529b41a32454f
Dar-ya218 <dasharoura13465@gmail.com>
2024-01-30 00:02:02 +0100

    add calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -9,3 +9,38 @@
 /*   Updated: 2024/01/25 12:24:51 by dabochko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+//#include <stdio.h>
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*result;
+
+	result = (void *)malloc(nmemb * size);
+	if (result == NULL)
+	{
+		errno = ENOMEM;
+		return ((void *)0);
+	}
+	else
+	{
+		ft_bzero(result, nmemb * size);
+		return (result);
+	}
+}
+
+/*int main() {
+	size_t nmemb = 5;
+	size_t size = 10;
+
+	char *result = ft_calloc(nmemb, size);
+
+	if (result) {
+		printf("Allocated %ld bytes.\n", nmemb * size);
+	} else {
+		printf("Failed to allocate %ld bytes.\n", nmemb * size);
+	}
+
+	return 0;
+}*/
