96ca4e70c17ef7b572e9fad6151686ae125cafaf
Dar-ya218 <dasharoura13465@gmail.com>
2024-02-02 00:07:27 +0100

    add putendl

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -11,3 +11,19 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+//#include <unistd.h>
+
+/*This function takes a string and a file descriptor, and writes the string to */
+/*the given file descriptor, followed by a newline.                            */
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	write(fd, s, ft_strlen(s));
+	write(fd, "\n", 1);
+}
+
+/*
+int main() {
+	ft_putendl_fd("Hello, World!", 1);
+	return 0;
+}*/
