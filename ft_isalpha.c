e34b3251fd7f505efdbc326c32609078e20c721c
Dar-ya218 <dasharoura13465@gmail.com>
2024-02-04 19:35:31 +0100

    main for isalpha and small changes

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -14,8 +14,15 @@
 
 int	ft_isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
 }
+
+/*
+int	main(void)
+{
+	printf("%d", ft_isalpha('a'));
+}
+*/
